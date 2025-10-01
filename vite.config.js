cf0e7b0c909f6289b033297d0c5fa1f40863136c
gusfring0101 <luisitomartinito02@gmail.com>
2025-10-02 01:12:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'dist'
-  }
-})
+  build: { outDir: "dist" }, // <- Vercel buscará este directorio
+});
+
+
