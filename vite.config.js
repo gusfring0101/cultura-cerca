29ef66ca9867f7b8a290bd0df8b604891a53e72a
gusfring0101 <luisitomartinito02@gmail.com>
2025-10-03 22:08:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // evita fallos por referencias a process.env en libs
+    'process.env': {}
+  }
 })
+
 
 
 
